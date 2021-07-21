1647ed56cef174666c495be18aff4e0217e5ae38
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2021-07-21 13:16:52 +0200

    Adapted the MPS tests for MPI builds.

--- tests/mps.cpp
+++ tests/mps.cpp
@@ -21,6 +21,8 @@ using std::string;
 
 #include "Util/Macros.hpp"
 
+#include "Util/Mpi.hpp"
+
 #ifdef XPED_CACHE_PERMUTE_OUTPUT
 #    pragma message("Xped is using LRU cache for the output of FusionTree manipulations.")
 #    include "lru/lru.hpp"
@@ -37,20 +39,42 @@ XPED_INIT_TREE_CACHE_VARIABLE(tree_cache, 100)
 #include "MPS/MpsAlgebra.hpp"
 
 #include "doctest/doctest.h"
+#ifdef XPED_USE_MPI
+#    include "doctest/extensions/doctest_mpi.h"
+#endif
 
 #include "mps_tests.hpp"
 
-constexpr std::size_t SU2_BASIS_SIZE = 2000;
+#ifdef XPED_USE_MPI
+constexpr std::size_t SU2_BASIS_SIZE = 100;
+constexpr std::size_t U1_BASIS_SIZE = 100;
+constexpr std::size_t U0_BASIS_SIZE = 100;
+#else
+constexpr std::size_t SU2_BASIS_SIZE = 5000;
 constexpr std::size_t U1_BASIS_SIZE = 1000;
-constexpr std::size_t U0_BASIS_SIZE = 500;
+constexpr std::size_t U0_BASIS_SIZE = 1000;
+#endif
+
+#ifdef XPED_USE_MPI
+constexpr int MPI_NUM_PROC = 2;
+#endif
 
 TEST_SUITE_BEGIN("Mps");
 
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing contract_L() and contract_R().", MPI_NUM_PROC)
+#else
 TEST_CASE("Testing contract_L() and contract_R().")
+#endif
 {
-    SUBCASE("SU2") { perform_mps_contraction<Sym::SU2<Sym::SpinSU2>>(SU2_BASIS_SIZE); }
-    SUBCASE("U1") { perform_mps_contraction<Sym::U1<Sym::SpinU1>>(U1_BASIS_SIZE); }
-    SUBCASE("U0") { perform_mps_contraction<Sym::U0<>>(U0_BASIS_SIZE); }
+#ifdef XPED_USE_MPI
+    util::mpi::XpedWorld test_world(test_comm);
+#else
+    util::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2") { perform_mps_contraction<Sym::SU2<Sym::SpinSU2>>(SU2_BASIS_SIZE, test_world); }
+    SUBCASE("U1") { perform_mps_contraction<Sym::U1<Sym::SpinU1>>(U1_BASIS_SIZE, test_world); }
+    SUBCASE("U0") { perform_mps_contraction<Sym::U0<>>(U0_BASIS_SIZE, test_world); }
 }
 
 TEST_SUITE_END();
--- tests/mps_tests.hpp
+++ tests/mps_tests.hpp
@@ -1,27 +1,34 @@
 template <typename Symmetry>
-void perform_mps_contraction(std::size_t basis_size)
+void perform_mps_contraction(std::size_t basis_size, util::mpi::XpedWorld& world)
 {
     Qbasis<Symmetry, 1> in, loc;
-    in.setRandom(basis_size);
-    loc.setRandom(8);
+    if(world.rank == 0) {
+        in.setRandom(basis_size);
+        loc.setRandom(8);
+    }
+    util::mpi::broadcast(in, world.rank, 0, world);
+    util::mpi::broadcast(loc, world.rank, 0, world);
+
     auto out = in.combine(loc).forgetHistory();
-    Xped<double, 2, 1, Symmetry> T1({{in, loc}}, {{out}});
+    Xped<double, 2, 1, Symmetry> T1({{in, loc}}, {{out}}, world);
     T1.setRandom();
-    Xped<double, 2, 1, Symmetry> T2({{in, loc}}, {{out}});
+    Xped<double, 2, 1, Symmetry> T2({{in, loc}}, {{out}}, world);
     T2.setRandom();
 
-    Xped<double, 1, 1, Symmetry> Bright({{out}}, {{out}});
+    Xped<double, 1, 1, Symmetry> Bright({{out}}, {{out}}, world);
     Bright.setRandom();
+    spdlog::get("info")->info("Mps tests: Set tensors to random.");
     Xped<double, 1, 1, Symmetry> Brightn;
     contract_R(Bright, T1, T2, Brightn);
     auto Bcheck = (T2 * Bright).template permute<+1, 0, 1, 2>() * (T1.template permute<+1, 0, 1, 2>().adjoint());
     CHECK((Brightn - Bcheck).norm() == doctest::Approx(0.));
-
-    Xped<double, 1, 1, Symmetry> Bleft({{in}}, {{in}});
+    spdlog::get("info")->info("Mps tests: right check done.");
+    Xped<double, 1, 1, Symmetry> Bleft({{in}}, {{in}}, world);
     Bleft.setRandom();
     Xped<double, 1, 1, Symmetry> Bleftn;
     contract_L(Bleft, T1, T2, Bleftn);
     Bcheck.clear();
     Bcheck = (T1.template permute<+1, 0, 1, 2>().adjoint() * Bleft).template permute<+1, 1, 2, 0>() * T2;
     CHECK((Bleftn - Bcheck).norm() == doctest::Approx(0.));
+    spdlog::get("info")->info("Mps tests: left check done.");
 }

81661b77af5278950d1e500dd198880e2b7180b0
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2021-12-03 11:59:36 +0100

    Put implementation of MPI header in source file + bugfix in broadcast.

--- include/Xped/Util/Mpi.hpp
+++ include/Xped/Util/Mpi.hpp
@@ -2,11 +2,7 @@
 #define XPED_MPI_H_
 
 #ifdef XPED_USE_MPI
-
 #    include "ctf.hpp"
-#    include "mpi.h"
-#    include "yas/serialize.hpp"
-
 namespace util::mpi {
 
 template <typename T>
@@ -16,35 +12,11 @@ struct TrivialDeleter
 };
 
 typedef CTF::World XpedWorld;
-XpedWorld& getUniverse() { return CTF::get_universe(); }
+inline XpedWorld& getUniverse() { return CTF::get_universe(); }
 
 template <typename T>
-void broadcast(T&& t, int process_rank, int root_process = 0, XpedWorld& world = getUniverse())
-{
-    constexpr std::size_t flags = yas::mem /*IO type*/ | yas::binary; /*IO format*/
-    yas::shared_buffer buf;
-    if(process_rank == root_process) { buf = yas::save<flags>(t); }
+void broadcast(T&& t, int process_rank, int root_process = 0, XpedWorld& world = getUniverse());
 
-    int size = 0;
-    char* plain_buf = NULL;
-    if(process_rank == root_process) {
-        size = buf.size;
-
-        plain_buf = (char*)malloc(sizeof(char) * size);
-        plain_buf = buf.data.get();
-    }
-
-    MPI_Bcast(&size, 1, MPI_INT, 0, world.comm);
-    if(process_rank != root_process) { plain_buf = (char*)malloc(sizeof(char) * size); }
-
-    MPI_Bcast(plain_buf, size, MPI_CHAR, 0, world.comm);
-
-    if(process_rank != root_process) {
-        buf = yas::shared_buffer(plain_buf, size);
-        yas::load<flags>(buf, t);
-    }
-    free(plain_buf);
-}
 } // namespace util::mpi
 #else
 namespace util::mpi {
@@ -65,11 +37,13 @@ inline XpedWorld universe{};
 inline XpedWorld& getUniverse() { return universe; };
 
 template <typename T>
-inline void broadcast(T&&, int, int, XpedWorld&)
-{
-    return;
-}
+void broadcast(T&&, int, int, XpedWorld&);
+
 } // namespace util::mpi
 
+#endif
+
+#ifndef XPED_COMPILED_LIB
+#    include "src/Mpi.hpp"
 #endif
 #endif
---
+++ src/Util/Mpi.cpp
@@ -0,0 +1,72 @@
+#include "Xped/Util/Mpi.hpp"
+
+#ifdef XPED_USE_MPI
+#    include "mpi.h"
+#    include "spdlog/spdlog.h"
+#    include "yas/serialize.hpp"
+
+#    include "Xped/Core/Qbasis.hpp"
+#    include "Xped/Symmetry/SU2.hpp"
+#    include "Xped/Symmetry/U0.hpp"
+#    include "Xped/Symmetry/U1.hpp"
+#    include "Xped/Util/Macros.hpp"
+namespace util::mpi {
+
+template <typename T>
+void broadcast(T&& t, int process_rank, int root_process, XpedWorld& world)
+{
+    XPED_MPI_BARRIER(world.comm)
+    SPDLOG_TRACE("Entering broadcast.");
+    constexpr std::size_t flags = yas::mem /*IO type*/ | yas::binary; /*IO format*/
+    yas::shared_buffer buf;
+    if(process_rank == root_process) {
+        buf = yas::save<flags>(t);
+        SPDLOG_TRACE("Root set buf.");
+    }
+
+    int size = 0;
+    char* plain_buf = nullptr;
+    if(process_rank == root_process) {
+        size = buf.size;
+
+        plain_buf = (char*)malloc(sizeof(char) * size);
+        std::memcpy(plain_buf, buf.data.get(), size);
+        SPDLOG_TRACE("Root malloced plain buf.");
+    }
+
+    MPI_Bcast(&size, 1, MPI_INT, root_process, world.comm);
+    SPDLOG_TRACE("Called MPI_Bcast for size.");
+    if(process_rank != root_process) {
+        plain_buf = (char*)malloc(sizeof(char) * size);
+        SPDLOG_TRACE("Non root malloced plain buf.");
+    }
+
+    MPI_Bcast(plain_buf, size, MPI_CHAR, root_process, world.comm);
+    SPDLOG_TRACE("Called MPI_Bcast for plain buf.");
+
+    if(process_rank != root_process) {
+        buf = yas::shared_buffer(plain_buf, size);
+        SPDLOG_TRACE("Non root set buf.");
+        yas::load<flags>(buf, t);
+    }
+    XPED_MPI_BARRIER(world.comm)
+    SPDLOG_TRACE("Before free.");
+    free(plain_buf);
+    XPED_MPI_BARRIER(world.comm)
+    SPDLOG_TRACE("After free.");
+    XPED_MPI_BARRIER(world.comm)
+    SPDLOG_TRACE("Leaving broadcast.");
+}
+
+} // namespace util::mpi
+#else
+namespace util::mpi {
+
+template <typename T>
+inline void broadcast(T&&, int, int, XpedWorld&)
+{
+    return;
+}
+
+} // namespace util::mpi
+#endif

b1e36b3e102d8e2314f426649df4b9764107c233
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2021-12-06 13:18:45 +0100

    Transferred definition of Eigen Matrix Interface into dedicated source file.

--- include/Xped/Interfaces/MatrixInterface_Eigen_impl.hpp
+++ include/Xped/Interfaces/MatrixInterface_Eigen_impl.hpp
@@ -1,13 +1,9 @@
-
 #ifndef MATRIX_INTERFACE_EIGEN_IMPL_H_
 #define MATRIX_INTERFACE_EIGEN_IMPL_H_
 
 #include "Eigen/Dense"
-#include "unsupported/Eigen/KroneckerProduct"
 
 #include "Xped/Util/Mpi.hpp"
-
-#include "Xped/Interfaces/MatrixMultiplication.hpp"
 
 template <>
 struct MatrixInterface<EigenMatrixLib>
@@ -26,161 +22,75 @@ struct MatrixInterface<EigenMatrixLib>
     typedef Eigen::Index MIndextype;
     // constructors
     template <typename Scalar>
-    static MType<Scalar> construct(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse())
-    {
-        return MType<Scalar>(rows, cols);
-    }
+    static MType<Scalar> construct(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse());
 
     template <typename Scalar>
-    static MType<Scalar> construct_with_zero(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse())
-    {
-        MType<Scalar> mat(rows, cols);
-        mat.setZero();
-        return mat;
-    }
+    static MType<Scalar> construct_with_zero(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse());
 
     template <typename Scalar>
-    static void resize(MType<Scalar>& M, const MIndextype& new_rows, const MIndextype& new_cols)
-    {
-        M.resize(new_rows, new_cols);
-    }
+    static void resize(MType<Scalar>& M, const MIndextype& new_rows, const MIndextype& new_cols);
 
     // initialization
     template <typename Scalar>
-    static void setZero(MType<Scalar>& M)
-    {
-        M.setZero();
-    }
+    static void setZero(MType<Scalar>& M);
 
     template <typename Scalar>
-    static void setRandom(MType<Scalar>& M)
-    {
-        M.setRandom();
-    }
+    static void setRandom(MType<Scalar>& M);
 
     template <typename Scalar>
-    static void setIdentity(MType<Scalar>& M)
-    {
-        M.setIdentity();
-    }
+    static void setIdentity(MType<Scalar>& M);
 
     template <typename Scalar>
-    static void setConstant(MType<Scalar>& M, const Scalar& val)
-    {
-        M.setConstant(val);
-    }
+    static void setConstant(MType<Scalar>& M, const Scalar& val);
 
     template <typename Scalar>
-    static MType<Scalar> Identity(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse())
-    {
-        return Eigen::MatrixXd::Identity(rows, cols);
-    }
+    static MType<Scalar> Identity(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world = util::mpi::getUniverse());
 
     // shape
     template <typename Scalar>
-    static MIndextype rows(const MType<Scalar>& M)
-    {
-        return M.rows();
-    }
+    static MIndextype rows(const MType<Scalar>& M);
 
     template <typename Scalar>
-    static MIndextype cols(const MType<Scalar>& M)
-    {
-        return M.cols();
-    }
+    static MIndextype cols(const MType<Scalar>& M);
 
     template <typename Scalar>
-    static Scalar getVal(const MType<Scalar>& M, const MIndextype& row, const MIndextype& col)
-    {
-        return M(row, col);
-    }
+    static Scalar getVal(const MType<Scalar>& M, const MIndextype& row, const MIndextype& col);
 
     // reduction
     template <typename Scalar>
-    static Scalar trace(const MType<Scalar>& M)
-    {
-        return M.trace();
-    }
+    static Scalar trace(const MType<Scalar>& M);
 
     // artithmetic
     template <typename Scalar>
-    static MType<Scalar> kronecker_prod(const MType<Scalar>& M1, const MType<Scalar>& M2)
-    {
-        return MType<Scalar>(Eigen::kroneckerProduct(M1, M2));
-    }
+    static MType<Scalar> kronecker_prod(const MType<Scalar>& M1, const MType<Scalar>& M2);
 
     template <typename Scalar>
-    static MType<Scalar> prod(const MType<Scalar>& M1, const MType<Scalar>& M2)
-    {
-        return MType<Scalar>(M1 * M2);
-    }
+    static MType<Scalar> prod(const MType<Scalar>& M1, const MType<Scalar>& M2);
 
     template <typename Scalar, typename MatrixExpr1, typename MatrixExpr2, typename MatrixExpr3, typename MatrixExprRes>
-    static void optimal_prod(const Scalar& scale, const MatrixExpr1& M1, const MatrixExpr2& M2, const MatrixExpr3& M3, MatrixExprRes& Mres)
-    {
-        std::vector<std::size_t> cost(2);
-        cost = internal::mult_cost(std::array<MIndextype, 2>{M1.rows(), M1.cols()},
-                                   std::array<MIndextype, 2>{M2.rows(), M2.cols()},
-                                   std::array<MIndextype, 2>{M3.rows(), M3.cols()});
-        std::size_t opt_mult = std::min_element(cost.begin(), cost.end()) - cost.begin();
-
-        if(opt_mult == 0) {
-            MType<Scalar> Mtmp = M1 * M2;
-            Mres.noalias() = scale * Mtmp * M3;
-        } else if(opt_mult == 1) {
-            MType<Scalar> Mtmp = M2 * M3;
-            Mres.noalias() = scale * M1 * Mtmp;
-        }
-    }
+    static void optimal_prod(const Scalar& scale, const MatrixExpr1& M1, const MatrixExpr2& M2, const MatrixExpr3& M3, MatrixExprRes& Mres);
 
     template <typename Scalar, typename MatrixExpr1, typename MatrixExpr2, typename MatrixExpr3, typename MatrixExprRes>
-    static void optimal_prod_add(const Scalar& scale, const MatrixExpr1& M1, const MatrixExpr2& M2, const MatrixExpr3& M3, MatrixExprRes& Mres)
-    {
-        std::vector<std::size_t> cost(2);
-        cost = internal::mult_cost(std::array<MIndextype, 2>{M1.rows(), M1.cols()},
-                                   std::array<MIndextype, 2>{M2.rows(), M2.cols()},
-                                   std::array<MIndextype, 2>{M3.rows(), M3.cols()});
-        std::size_t opt_mult = std::min_element(cost.begin(), cost.end()) - cost.begin();
-
-        if(opt_mult == 0) {
-            MType<Scalar> Mtmp = M1 * M2;
-            Mres.noalias() += scale * Mtmp * M3;
-        } else if(opt_mult == 1) {
-            MType<Scalar> Mtmp = M2 * M3;
-            Mres.noalias() += scale * M1 * Mtmp;
-        }
-    }
+    static void optimal_prod_add(const Scalar& scale, const MatrixExpr1& M1, const MatrixExpr2& M2, const MatrixExpr3& M3, MatrixExprRes& Mres);
 
     template <typename Scalar>
-    static auto add(const MType<Scalar>& M1, const MType<Scalar>& M2)
-    {
-        return (M1 + M2);
-    }
+    static const Eigen::CwiseBinaryOp<Eigen::internal::scalar_sum_op<Scalar, Scalar>, const MType<Scalar>, const MType<Scalar>>
+    add(const MType<Scalar>& M1, const MType<Scalar>& M2);
 
     template <typename Scalar>
-    static auto difference(const MType<Scalar>& M1, const MType<Scalar>& M2)
-    {
-        return (M1 - M2);
-    }
+    static const Eigen::CwiseBinaryOp<Eigen::internal::scalar_difference_op<Scalar, Scalar>, const MType<Scalar>, const MType<Scalar>>
+    difference(const MType<Scalar>& M1, const MType<Scalar>& M2);
 
     template <typename Scalar>
-    static void scale(MType<Scalar>& M, const Scalar& val)
-    {
-        M = (val * M);
-    }
+    static void scale(MType<Scalar>& M, const Scalar& val);
 
     template <typename Scalar>
-    static auto adjoint(const MType<Scalar>& M)
-    {
-        return M.adjoint();
-    }
+    static typename Eigen::MatrixBase<MType<Scalar>>::AdjointReturnType adjoint(const MType<Scalar>& M);
 
     // block
     template <typename Scalar>
-    static auto block(const MType<Scalar>& M, const MIndextype& row_off, const MIndextype& col_off, const MIndextype& rows, const MIndextype& cols)
-    {
-        return M.block(row_off, col_off, rows, cols);
-    }
+    static const Eigen::Block<const MType<Scalar>>
+    block(const MType<Scalar>& M, const MIndextype& row_off, const MIndextype& col_off, const MIndextype& rows, const MIndextype& cols);
 
     template <typename Scalar>
     static void add_to_block(MType<Scalar>& M1,
@@ -188,10 +98,7 @@ struct MatrixInterface<EigenMatrixLib>
                              const MIndextype& col_off,
                              const MIndextype& rows,
                              const MIndextype& cols,
-                             const MType<Scalar>& M2)
-    {
-        M1.block(row_off, col_off, rows, cols) += M2;
-    }
+                             const MType<Scalar>& M2);
 
     template <typename Scalar>
     static void set_block(MType<Scalar>& M1,
@@ -199,18 +106,14 @@ struct MatrixInterface<EigenMatrixLib>
                           const MIndextype& col_off,
                           const MIndextype& rows,
                           const MIndextype& cols,
-                          const MType<Scalar>& M2)
-    {
-        M1.block(row_off, col_off, rows, cols) = M2;
-    }
+                          const MType<Scalar>& M2);
 
     template <typename Scalar>
-    static std::string print(const MType<Scalar>& M)
-    {
-        std::stringstream ss;
-        ss << M;
-        return ss.str();
-    }
+    static std::string print(const MType<Scalar>& M);
 };
+
+#ifndef XPED_COMPILED_LIB
+#    include "Interfaces/MatrixInterface_Eigen_impl.cpp"
+#endif
 
 #endif
---
+++ src/Interfaces/MatrixInterface_Eigen_impl.cpp
@@ -0,0 +1,216 @@
+#include "Xped/Interfaces/MatrixInterface.hpp"
+
+#include "Xped/Interfaces/MatrixInterface_Eigen_impl.hpp"
+
+#include "unsupported/Eigen/KroneckerProduct"
+
+#include "Xped/Util/Mpi.hpp"
+
+#include "Xped/Interfaces/MatrixMultiplication.hpp"
+
+template <typename Scalar>
+using MType = MatrixInterface<EigenMatrixLib>::MType<Scalar>;
+
+using MIndextype = MatrixInterface<EigenMatrixLib>::MIndextype;
+
+// constructors
+template <typename Scalar>
+MType<Scalar> MatrixInterface<EigenMatrixLib>::construct(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world)
+{
+    return MType<Scalar>(rows, cols);
+}
+
+template <typename Scalar>
+MType<Scalar> MatrixInterface<EigenMatrixLib>::construct_with_zero(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world)
+{
+    MType<Scalar> mat(rows, cols);
+    mat.setZero();
+    return mat;
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::resize(MType<Scalar>& M, const MIndextype& new_rows, const MIndextype& new_cols)
+{
+    M.resize(new_rows, new_cols);
+}
+
+// initialization
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::setZero(MType<Scalar>& M)
+{
+    M.setZero();
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::setRandom(MType<Scalar>& M)
+{
+    M.setRandom();
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::setIdentity(MType<Scalar>& M)
+{
+    M.setIdentity();
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::setConstant(MType<Scalar>& M, const Scalar& val)
+{
+    M.setConstant(val);
+}
+
+template <typename Scalar>
+MType<Scalar> MatrixInterface<EigenMatrixLib>::Identity(const MIndextype& rows, const MIndextype& cols, util::mpi::XpedWorld& world)
+{
+    return Eigen::MatrixXd::Identity(rows, cols);
+}
+
+// shape
+template <typename Scalar>
+MIndextype MatrixInterface<EigenMatrixLib>::rows(const MType<Scalar>& M)
+{
+    return M.rows();
+}
+
+template <typename Scalar>
+MIndextype MatrixInterface<EigenMatrixLib>::cols(const MType<Scalar>& M)
+{
+    return M.cols();
+}
+
+template <typename Scalar>
+Scalar MatrixInterface<EigenMatrixLib>::getVal(const MType<Scalar>& M, const MIndextype& row, const MIndextype& col)
+{
+    return M(row, col);
+}
+
+// reduction
+template <typename Scalar>
+Scalar MatrixInterface<EigenMatrixLib>::trace(const MType<Scalar>& M)
+{
+    return M.trace();
+}
+
+// artithmetic
+template <typename Scalar>
+MType<Scalar> MatrixInterface<EigenMatrixLib>::kronecker_prod(const MType<Scalar>& M1, const MType<Scalar>& M2)
+{
+    return MType<Scalar>(Eigen::kroneckerProduct(M1, M2));
+}
+
+template <typename Scalar>
+MType<Scalar> MatrixInterface<EigenMatrixLib>::prod(const MType<Scalar>& M1, const MType<Scalar>& M2)
+{
+    return MType<Scalar>(M1 * M2);
+}
+
+template <typename Scalar, typename MatrixExpr1, typename MatrixExpr2, typename MatrixExpr3, typename MatrixExprRes>
+void MatrixInterface<EigenMatrixLib>::optimal_prod(const Scalar& scale,
+                                                   const MatrixExpr1& M1,
+                                                   const MatrixExpr2& M2,
+                                                   const MatrixExpr3& M3,
+                                                   MatrixExprRes& Mres)
+{
+    std::vector<std::size_t> cost(2);
+    cost = internal::mult_cost(std::array<MIndextype, 2>{M1.rows(), M1.cols()},
+                               std::array<MIndextype, 2>{M2.rows(), M2.cols()},
+                               std::array<MIndextype, 2>{M3.rows(), M3.cols()});
+    std::size_t opt_mult = std::min_element(cost.begin(), cost.end()) - cost.begin();
+
+    if(opt_mult == 0) {
+        MType<Scalar> Mtmp = M1 * M2;
+        Mres.noalias() = scale * Mtmp * M3;
+    } else if(opt_mult == 1) {
+        MType<Scalar> Mtmp = M2 * M3;
+        Mres.noalias() = scale * M1 * Mtmp;
+    }
+}
+
+template <typename Scalar, typename MatrixExpr1, typename MatrixExpr2, typename MatrixExpr3, typename MatrixExprRes>
+void MatrixInterface<EigenMatrixLib>::optimal_prod_add(const Scalar& scale,
+                                                       const MatrixExpr1& M1,
+                                                       const MatrixExpr2& M2,
+                                                       const MatrixExpr3& M3,
+                                                       MatrixExprRes& Mres)
+{
+    std::vector<std::size_t> cost(2);
+    cost = internal::mult_cost(std::array<MIndextype, 2>{M1.rows(), M1.cols()},
+                               std::array<MIndextype, 2>{M2.rows(), M2.cols()},
+                               std::array<MIndextype, 2>{M3.rows(), M3.cols()});
+    std::size_t opt_mult = std::min_element(cost.begin(), cost.end()) - cost.begin();
+
+    if(opt_mult == 0) {
+        MType<Scalar> Mtmp = M1 * M2;
+        Mres.noalias() += scale * Mtmp * M3;
+    } else if(opt_mult == 1) {
+        MType<Scalar> Mtmp = M2 * M3;
+        Mres.noalias() += scale * M1 * Mtmp;
+    }
+}
+
+template <typename Scalar>
+const Eigen::CwiseBinaryOp<Eigen::internal::scalar_sum_op<Scalar, Scalar>, const MType<Scalar>, const MType<Scalar>>
+MatrixInterface<EigenMatrixLib>::add(const MType<Scalar>& M1, const MType<Scalar>& M2)
+{
+    return (M1 + M2);
+}
+
+template <typename Scalar>
+const Eigen::CwiseBinaryOp<Eigen::internal::scalar_difference_op<Scalar, Scalar>, const MType<Scalar>, const MType<Scalar>>
+MatrixInterface<EigenMatrixLib>::difference(const MType<Scalar>& M1, const MType<Scalar>& M2)
+{
+    return (M1 - M2);
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::scale(MType<Scalar>& M, const Scalar& val)
+{
+    M = (val * M);
+}
+
+template <typename Scalar>
+typename Eigen::MatrixBase<MType<Scalar>>::AdjointReturnType MatrixInterface<EigenMatrixLib>::adjoint(const MType<Scalar>& M)
+{
+    return M.adjoint();
+}
+
+// block
+template <typename Scalar>
+const Eigen::Block<const MType<Scalar>> MatrixInterface<EigenMatrixLib>::block(const MType<Scalar>& M,
+                                                                               const MIndextype& row_off,
+                                                                               const MIndextype& col_off,
+                                                                               const MIndextype& rows,
+                                                                               const MIndextype& cols)
+{
+    return M.block(row_off, col_off, rows, cols);
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::add_to_block(MType<Scalar>& M1,
+                                                   const MIndextype& row_off,
+                                                   const MIndextype& col_off,
+                                                   const MIndextype& rows,
+                                                   const MIndextype& cols,
+                                                   const MType<Scalar>& M2)
+{
+    M1.block(row_off, col_off, rows, cols) += M2;
+}
+
+template <typename Scalar>
+void MatrixInterface<EigenMatrixLib>::set_block(MType<Scalar>& M1,
+                                                const MIndextype& row_off,
+                                                const MIndextype& col_off,
+                                                const MIndextype& rows,
+                                                const MIndextype& cols,
+                                                const MType<Scalar>& M2)
+{
+    M1.block(row_off, col_off, rows, cols) = M2;
+}
+
+template <typename Scalar>
+std::string MatrixInterface<EigenMatrixLib>::print(const MType<Scalar>& M)
+{
+    std::stringstream ss;
+    ss << M;
+    return ss.str();
+}

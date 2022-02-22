4e06d38c62bda0f28861827f5956581da8a4cfb5
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-02-22 13:26:54 +0100

    Removed preprocessor debug output.

--- include/Xped/Core/storage/StorageType.hpp
+++ include/Xped/Core/storage/StorageType.hpp
@@ -4,7 +4,6 @@
 #if defined XPED_USE_CONTIGOUS_STORAGE
 #    include "Xped/Core/storage/StorageType_contigous.hpp"
 #elif defined XPED_USE_VECOFMAT_STORAGE
-#    pragma message("VecOfMat storage")
 #    include "Xped/Core/storage/StorageType_vecofmat.hpp"
 #endif
 

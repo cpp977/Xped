43935e9c3ea1ac64cb4bfe6f570fede1deb91e1b
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-03-03 12:06:38 +0100

    Fixed typo.
    contigous -> contiguous.

--- include/Xped/Core/storage/StorageType.hpp
+++ include/Xped/Core/storage/StorageType.hpp
@@ -1,8 +1,8 @@
 #ifndef XPED_STORAGE_TYPE_HPP_
 #define XPED_STORAGE_TYPE_HPP_
 
-#if defined XPED_USE_CONTIGOUS_STORAGE
-#    include "Xped/Core/storage/StorageType_contigous.hpp"
+#if defined XPED_USE_CONTIGUOUS_STORAGE
+#    include "Xped/Core/storage/StorageType_contiguous.hpp"
 #elif defined XPED_USE_VECOFMAT_STORAGE
 #    include "Xped/Core/storage/StorageType_vecofmat.hpp"
 #endif

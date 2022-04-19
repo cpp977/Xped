964d9858aaf128a41801526648b2aaf4e99fa52d
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-04-19 08:34:53 +0200

    Added constexpr function to compute the inverse permutation.

--- include/Xped/Util/Constfct.hpp
+++ include/Xped/Util/Constfct.hpp
@@ -1,5 +1,7 @@
-#ifndef CONSTFCT_H_
-#define CONSTFCT_H_
+#ifndef XPED_CONSTFCT_H_
+#define XPED_CONSTFCT_H_
+
+#include "seq/seq.h"
 
 namespace Xped::util::constFct {
 
@@ -19,6 +21,18 @@ template <std::size_t Rank>
 constexpr bool isSmaller(std::size_t x, std::size_t /*unused*/)
 {
     return x < Rank;
+}
+
+template <typename Perm, typename Perm::value_type... Is>
+constexpr auto inverse_perm_impl(seq::iseq<typename Perm::value_type, Is...>)
+{
+    return seq::iseq<typename Perm::value_type, seq::index_of<Is, Perm>...>{};
+}
+
+template <typename Perm, typename Indices = seq::make<typename Perm::value_type, Perm::size()>>
+constexpr auto inverse_permutation()
+{
+    return inverse_perm_impl<Perm>(Indices{});
 }
 
 } // namespace Xped::util::constFct

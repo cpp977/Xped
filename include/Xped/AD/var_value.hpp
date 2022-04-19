d9566a94c7757b92d659ac8800852c4a93c7fb37
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-04-19 08:44:10 +0200

    Added AD functionality for tensors not living in the stan memory arena.
    This is achieved by additionally inheriting `stan::math::chainable_alloc` from `vari_value`. Works out well.

--- include/Xped/AD/var_value.hpp
+++ include/Xped/AD/var_value.hpp
@@ -4,6 +4,30 @@
 #include "Xped/Core/Tensor.hpp"
 
 template <typename T>
+struct is_tensor
+{
+    static const bool value = false;
+};
+
+template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry, typename AllocationPolicy>
+struct is_tensor<Xped::Tensor<Scalar, Rank, CoRank, Symmetry, AllocationPolicy>>
+{
+    static const bool value = true;
+};
+
+template <typename T>
+struct is_tensor_var
+{
+    static const bool value = false;
+};
+
+template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
+struct is_tensor_var<stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>>
+{
+    static const bool value = true;
+};
+
+template <typename T>
 struct is_arena_tensor
 {
     static const bool value = false;
@@ -28,16 +52,28 @@ struct is_arena_tensor_var<stan::math::var_value<Xped::ArenaTensor<Scalar, Rank,
 };
 
 template <typename T>
-using require_tensor_v = stan::require_t<stan::bool_constant<is_arena_tensor<T>::value>>;
+using require_tensor_v = stan::require_t<stan::bool_constant<is_tensor<T>::value>>;
 
 template <typename T>
-using require_tensor_t = stan::bool_constant<is_arena_tensor<T>::value>;
+using require_tensor_t = stan::bool_constant<is_tensor<T>::value>;
 
 template <typename T>
-using require_tensor_var_t = stan::bool_constant<is_arena_tensor_var<T>::value>;
+using require_tensor_var_t = stan::bool_constant<is_tensor_var<T>::value>;
 
 template <typename T>
-using require_tensor_var_v = stan::require_t<stan::bool_constant<is_arena_tensor_var<T>::value>>;
+using require_tensor_var_v = stan::require_t<stan::bool_constant<is_tensor_var<T>::value>>;
+
+template <typename T>
+using require_arena_tensor_v = stan::require_t<stan::bool_constant<is_arena_tensor<T>::value>>;
+
+template <typename T>
+using require_arena_tensor_t = stan::bool_constant<is_arena_tensor<T>::value>;
+
+template <typename T>
+using require_arena_tensor_var_t = stan::bool_constant<is_arena_tensor_var<T>::value>;
+
+template <typename T>
+using require_arena_tensor_var_v = stan::require_t<stan::bool_constant<is_arena_tensor_var<T>::value>>;
 
 #include "vari_value.hpp"
 
@@ -46,7 +82,7 @@ template <typename T>
 class var_value<T, require_tensor_v<T>>
 {
 public:
-    using value_type = T; // type in vari_value -->ArenaTensor.
+    using value_type = T; // type in vari_value -->ArenaTensor or Tensor.
     using vari_type = vari_value<value_type>;
 
     vari_type* vi_;
@@ -86,12 +122,24 @@ public:
 
     inline vari_type* operator->() { return vi_; }
 
-    // auto adjoint() const
-    // {
-    //     using vari_type = decltype(vi_->adjoint());
-    //     var_value<typename vari_type::value_type> out(new vari_type(vi_->adjoint()));
-    //     return out;
-    // }
+    auto adjoint() const
+    {
+        var_value<Xped::Tensor<typename T::Scalar, T::CoRank, T::Rank, typename T::Symmetry, typename T::AllocationPolicy>> out(
+            val().adjoint().eval());
+        stan::math::reverse_pass_callback([this_adj = this->adj(), out]() mutable { this_adj += out.adj().adjoint().eval(); });
+        return out;
+    }
+
+    template <int shift, std::size_t... p>
+    auto permute() const
+    {
+        var_value<Xped::Tensor<typename T::Scalar, T::Rank - shift, T::CoRank + shift, typename T::Symmetry, typename T::AllocationPolicy>> out(
+            val().template permute<shift, p...>());
+        stan::math::reverse_pass_callback([this_adj = this->adj(), out]() mutable {
+            using inverse = decltype(Xped::util::constFct::inverse_permutation<seq::iseq<std::size_t, p...>>());
+            this_adj += out.adj().template permute<-shift>(inverse{});
+        });
+    }
 
     template <typename Scalar>
     var_value& operator-=(const Scalar s)
@@ -108,43 +156,43 @@ public:
 };
 
 template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
-stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>
-operator-(const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
+stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>
+operator-(const stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
 {
-    stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>> out(t.val() - s);
+    stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>> out(t.val() - s);
     return out;
 }
 
 template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
-stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>
-operator+(const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
+stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>
+operator+(const stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
 {
-    stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>> out(t.val() + s);
+    stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>> out(t.val() + s);
     return out;
 }
 
 template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
-stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>
-operator*(const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
+stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>
+operator*(const stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>& t, Scalar s)
 {
-    stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>> out((t.val() * s).eval());
+    stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>> out((t.val() * s).eval());
     stan::math::reverse_pass_callback([out, t, s]() mutable { t.adj() += (out.adj() * s).eval(); });
     return out;
 }
 
 template <typename Scalar, std::size_t Rank, std::size_t MiddleRank, std::size_t CoRank, typename Symmetry>
-stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>
-operator*(const Xped::ArenaTensor<Scalar, Rank, MiddleRank, Symmetry>& left,
-          const stan::math::var_value<Xped::ArenaTensor<Scalar, MiddleRank, CoRank, Symmetry>>& right)
+stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>
+operator*(const Xped::Tensor<Scalar, Rank, MiddleRank, Symmetry>& left,
+          const stan::math::var_value<Xped::Tensor<Scalar, MiddleRank, CoRank, Symmetry>>& right)
 {
-    stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>> res(left * right.val());
+    stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>> res(left * right.val());
     stan::math::reverse_pass_callback([res, left, right]() mutable { right.adj() += (left.adjoint() * res.adj()); });
     return res;
 }
 
 template <typename Scalar, std::size_t Rank, typename Symmetry>
-stan::math::var operator*(const stan::math::var_value<Xped::ArenaTensor<Scalar, 0, Rank, Symmetry>>& left,
-                          const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, 0, Symmetry>>& right)
+stan::math::var operator*(const stan::math::var_value<Xped::Tensor<Scalar, 0, Rank, Symmetry>>& left,
+                          const stan::math::var_value<Xped::Tensor<Scalar, Rank, 0, Symmetry>>& right)
 {
     stan::math::var res = (left.val() * right.val()).block(0)(0, 0);
     stan::math::reverse_pass_callback([res, left, right]() mutable {
@@ -155,22 +203,22 @@ stan::math::var operator*(const stan::math::var_value<Xped::ArenaTensor<Scalar, 
 }
 
 template <typename Scalar, std::size_t Rank, typename Symmetry>
-stan::math::var operator*(const Xped::ArenaTensor<Scalar, 0, Rank, Symmetry>& left,
-                          const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, 0, Symmetry>>& right)
+stan::math::var operator*(const Xped::Tensor<Scalar, 0, Rank, Symmetry>& left,
+                          const stan::math::var_value<Xped::Tensor<Scalar, Rank, 0, Symmetry>>& right)
 {
     stan::math::var res = (left * right.val()).block(0)(0, 0);
     stan::math::reverse_pass_callback([res, left, right]() mutable { right.adj() += left.adjoint() * res.adj(); });
     return res;
 }
 
-template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
-stan::math::var_value<Xped::ArenaTensor<Scalar, CoRank, Rank, Symmetry>>
-adjoint(const stan::math::var_value<Xped::ArenaTensor<Scalar, Rank, CoRank, Symmetry>>& t)
-{
-    stan::math::var_value<Xped::ArenaTensor<Scalar, CoRank, Rank, Symmetry>> res(t.val().adjoint().eval());
-    stan::math::reverse_pass_callback([res, t]() mutable { t.adj() += res.adj().adjoint().eval(); });
-    return res;
-}
+// template <typename Scalar, std::size_t Rank, std::size_t CoRank, typename Symmetry>
+// stan::math::var_value<Xped::Tensor<Scalar, CoRank, Rank, Symmetry>>
+// adjoint(const stan::math::var_value<Xped::Tensor<Scalar, Rank, CoRank, Symmetry>>& t)
+// {
+//     stan::math::var_value<Xped::Tensor<Scalar, CoRank, Rank, Symmetry>> res(t.val().adjoint().eval());
+//     stan::math::reverse_pass_callback([res, t]() mutable { t.adj() += res.adj().adjoint().eval(); });
+//     return res;
+// }
 
 } // namespace stan::math
 #endif

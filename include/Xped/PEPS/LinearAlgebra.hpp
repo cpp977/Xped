713dfcb71d7e79918a783583d62ccd1f2ad57813
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-05-13 15:03:23 +0200

    Added `avg()` to compute the expectation value of a two-site observable using CTM env.

---
+++ include/Xped/PEPS/LinearAlgebra.hpp
@@ -0,0 +1,23 @@
+#ifndef XPED_PEPS_LINEAR_ALGEBRA_HPP_
+#define XPED_PEPS_LINEAR_ALGEBRA_HPP_
+
+#include <tuple>
+
+#include "Xped/Util/Macros.hpp"
+
+namespace Xped {
+
+template <typename Scalar_, typename Symmetry_, bool ENABLE_AD>
+class CTM;
+
+template <typename Scalar, typename Symmetry, bool ENABLE_AD>
+std::pair<PlainInterface::MType<std::conditional_t<ENABLE_AD, stan::math::var, Scalar>>,
+          PlainInterface::MType<std::conditional_t<ENABLE_AD, stan::math::var, Scalar>>>
+avg(XPED_CONST CTM<Scalar, Symmetry, ENABLE_AD>& env, XPED_CONST Tensor<Scalar, 2, 2, Symmetry, false>& op);
+
+} // namespace Xped
+
+#ifndef XPED_COMPILED_LIB
+#    include "PEPS/LinearAlgebra.cpp"
+#endif
+#endif

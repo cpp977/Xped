180dd9e89af678d5af98e490bf4fe440d0df47fc
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-05-22 11:45:35 +0200

    Corrected download hash for sundials 6.0.0.
    It seems that the hash has changed over time...

--- ports/sundials/portfile.cmake
+++ ports/sundials/portfile.cmake
@@ -2,7 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO LLNL/sundials
     REF v6.0.0
-    SHA512 a009bc77f31ad426cf02670f06363058ef83dfd6fd84c868e4c8713ccb453ceff481f98266b46c7a6de0ef4d4ecca74a8c8e78150b88cecc7cce41ed8f056dbb
+    # SHA512 a009bc77f31ad426cf02670f06363058ef83dfd6fd84c868e4c8713ccb453ceff481f98266b46c7a6de0ef4d4ecca74a8c8e78150b88cecc7cce41ed8f056dbb
+    SHA512 14e1b42aa6a1bb1c54a13bf2b1a9c5a6ab92bf8017878ee67e0d4ef22d58d9d41fd6fd439877216ebe86b7f1d98c6f27049b60b87741e9127d46eafb238eedda
     HEAD_REF main
     PATCHES
     install-dlls-in-bin.patch

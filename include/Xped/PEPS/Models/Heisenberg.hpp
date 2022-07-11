b1d394690802eb6df50549636cf53d31091a4965
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-07-11 10:32:03 +0200

    Cleaned up files and merge implementation for SU(2) and other symmetries.

--- include/Xped/PEPS/Models/Heisenberg.hpp
+++ include/Xped/PEPS/Models/Heisenberg.hpp
@@ -1,44 +1,49 @@
 #ifndef XPED_PEPS_HEISENBERG_HPP_
 #define XPED_PEPS_HEISENBERG_HPP_
 
-#include "Xped/Core/Tensor.hpp"
+#include <any>
+#include <map>
+#include <string>
 
+#include "Xped/Core/Tensor.hpp"
 #include "Xped/Physics/SpinBase.hpp"
 
 namespace Xped {
 
+/* gs energy:
+   square: E/N = -0.66944
+   triang: E/N = −0.5445
+     j1j2: E/N =
+*/
 template <typename Symmetry>
-class Heisenberg
+class Heisenberg : public TwoSiteObservable<Symmetry>
 {
 public:
-    static Tensor<double, 2, 2, Symmetry> twoSiteHamiltonian(double Jxy = 1., double Jz = 1.)
+    Heisenberg(std::map<std::string, std::any>& params, const Pattern& pat, Opts::Bond bond = Opts::Bond::H | Opts::Bond::V)
+        : TwoSiteObservable<Symmetry>(pat, bond)
     {
+        this->name = "Heisenberg";
         SpinBase<Symmetry> B(1, 2);
-        Tensor<double, 2, 2, Symmetry> out = Jz * tprod(B.Sz(), B.Sz()) + 0.5 * Jxy * (tprod(B.Sp(), B.Sm()) + tprod(B.Sm(), B.Sp()));
-        return out;
-    }
-};
-// gs energy: E/N = -0.66944
+        Tensor<double, 2, 2, Symmetry> gate;
+        if constexpr(std::is_same_v<Symmetry, Sym::SU2<Sym::SpinSU2>>) {
+            gate = std::any_cast<double>(params["J"]) * (std::sqrt(3.) * tprod(B.Sdag(0), B.S(0))).eval();
+        } else {
+            gate = std::any_cast<double>(params["Jz"]) * tprod(B.Sz(), B.Sz()) +
+                   0.5 * std::any_cast<double>(params["Jxy"]) * (tprod(B.Sp(), B.Sm()) + tprod(B.Sm(), B.Sp()));
+        }
 
-template <>
-class Heisenberg<Sym::SU2<Sym::SpinSU2>>
-{
-    using Symmetry = Sym::SU2<Sym::SpinSU2>;
-
-public:
-    static Tensor<double, 2, 2, Symmetry> twoSiteHamiltonian(double J = 1.)
-    {
-        Qbasis<Symmetry, 1> phys;
-        phys.push_back({2}, 1);
-        Qbasis<Symmetry, 1> triplet;
-        triplet.push_back({3}, 1);
-        auto fuse = Tensor<double, 2, 1, Symmetry>::Identity({{phys, phys}}, {{phys.combine(phys).forgetHistory()}});
-
-        Tensor<double, 2, 1, Symmetry> S({{phys, triplet}}, {{phys}});
-        S.setConstant(std::sqrt(0.75));
-
-        Tensor<double, 2, 2, Symmetry> out = J * S.template contract<std::array{-1, 1, -3}, std::array{-2, -4, 1}, 2>(S.adjoint().eval());
-        return out;
+        if((bond & Opts::Bond::H) == Opts::Bond::H) {
+            for(auto& t : this->data_h) { t = gate; }
+        }
+        if((bond & Opts::Bond::V) == Opts::Bond::V) {
+            for(auto& t : this->data_v) { t = gate; }
+        }
+        if((bond & Opts::Bond::D1) == Opts::Bond::D1) {
+            for(auto& t : this->data_d1) { t = std::any_cast<double>(params["J2"]) * gate; }
+        }
+        if((bond & Opts::Bond::D2) == Opts::Bond::D2) {
+            for(auto& t : this->data_d2) { t = std::any_cast<double>(params["J2"]) * gate; }
+        }
     }
 };
 
--- include/Xped/PEPS/Models/KondoNecklace.hpp
+++ include/Xped/PEPS/Models/KondoNecklace.hpp
@@ -1,64 +1,74 @@
 #ifndef XPED_KONDO_NECKLACE_HPP_
 #define XPED_KONDO_NECKLACE_HPP_
 
+#include <any>
+#include <map>
+#include <string>
+
 #include "Xped/Core/Tensor.hpp"
 #include "Xped/Physics/SpinBase.hpp"
 
 namespace Xped {
 
 template <typename Symmetry>
-class KondoNecklace
+class KondoNecklace : public TwoSiteObservable<Symmetry>
 {
 public:
-    static Tensor<double, 2, 2, Symmetry>
-    twoSiteHamiltonian(double Jkxy = 1., double Jkz = 1., double Jxy = 1., double Jz = 1., double Ixy = 0., double Iz = 0.)
+    KondoNecklace(std::map<std::string, std::any>& params, const Pattern& pat, Opts::Bond bond = Opts::Bond::H | Opts::Bond::V)
+        : TwoSiteObservable<Symmetry>(pat, bond)
     {
-
+        this->name = "KondoNecklace";
         SpinBase<Symmetry> B(2, 2);
-        auto SzSz = tprod(B.Sz(0), B.Sz(0));
-        auto SpSm = tprod(B.Sp(0), B.Sm(0));
-        auto SmSp = tprod(B.Sm(0), B.Sp(0));
+        Tensor<double, 2, 2, Symmetry> gate;
+        if constexpr(std::is_same_v<Symmetry, Sym::SU2<Sym::SpinSU2>>) {
+            auto SS = (std::sqrt(3.) * tprod(B.Sdag(0), B.S(0))).eval();
 
-        auto szsz = tprod(B.Sz(1), B.Sz(1));
-        auto spsm = tprod(B.Sp(1), B.Sm(1));
-        auto smsp = tprod(B.Sm(1), B.Sp(1));
+            auto ss = (std::sqrt(3.) * tprod(B.Sdag(1), B.S(1))).eval();
 
-        auto IdxSzsz = tprod(B.Id(), B.Sz(0) * B.Sz(1));
-        auto IdxSpsm = tprod(B.Id(), B.Sp(0) * B.Sm(1));
-        auto IdxSmsp = tprod(B.Id(), B.Sm(0) * B.Sp(1));
+            auto IdxSs = tprod(B.Id(), std::sqrt(3.) * SiteOperator<double, Symmetry>::prod(B.Sdag(0), B.S(1), Symmetry::qvacuum()));
 
-        auto SzszxId = tprod(B.Sz(0) * B.Sz(1), B.Id());
-        auto SpsmxId = tprod(B.Sp(0) * B.Sm(1), B.Id());
-        auto SmspxId = tprod(B.Sm(0) * B.Sp(1), B.Id());
+            auto SsxId = tprod(std::sqrt(3.) * SiteOperator<double, Symmetry>::prod(B.Sdag(0), B.S(1), Symmetry::qvacuum()), B.Id());
 
-        Tensor<double, 2, 2, Symmetry> out =
-            Jz * szsz + 0.5 * Jxy * spsm + 0.5 * Jxy * smsp +
-            0.25 * (Jkz * (SzszxId + IdxSzsz) + 0.5 * Jkxy * (SpsmxId + IdxSpsm) + 0.5 * Jkxy * (SmspxId + IdxSmsp)) + Iz * SzSz + 0.5 * Ixy * SpSm +
-            0.5 * Ixy * SmSp;
+            gate = std::any_cast<double>(params["J"]) * ss + 0.25 * std::any_cast<double>(params["Jk"]) * (SsxId + IdxSs) +
+                   std::any_cast<double>(params["I"]) * SS;
+        } else {
+            auto SzSz = tprod(B.Sz(0), B.Sz(0));
+            auto SpSm = tprod(B.Sp(0), B.Sm(0));
+            auto SmSp = tprod(B.Sm(0), B.Sp(0));
 
-        return out;
-    }
-};
+            auto szsz = tprod(B.Sz(1), B.Sz(1));
+            auto spsm = tprod(B.Sp(1), B.Sm(1));
+            auto smsp = tprod(B.Sm(1), B.Sp(1));
 
-template <>
-class KondoNecklace<Sym::SU2<Sym::SpinSU2>>
-{
-    using Symmetry = Sym::SU2<Sym::SpinSU2>;
+            auto IdxSzsz = tprod(B.Id(), B.Sz(0) * B.Sz(1));
+            auto IdxSpsm = tprod(B.Id(), B.Sp(0) * B.Sm(1));
+            auto IdxSmsp = tprod(B.Id(), B.Sm(0) * B.Sp(1));
 
-public:
-    static Tensor<double, 2, 2, Symmetry> twoSiteHamiltonian(double Jk, double J = 1., double I = 0.)
-    {
-        SpinBase<Symmetry> B(2, 2);
-        auto SS = (std::sqrt(3.) * tprod(B.Sdag(0), B.S(0))).eval();
+            auto SzszxId = tprod(B.Sz(0) * B.Sz(1), B.Id());
+            auto SpsmxId = tprod(B.Sp(0) * B.Sm(1), B.Id());
+            auto SmspxId = tprod(B.Sm(0) * B.Sp(1), B.Id());
 
-        auto ss = (std::sqrt(3.) * tprod(B.Sdag(1), B.S(1))).eval();
+            gate = std::any_cast<double>(params["Jz"]) * szsz + 0.5 * std::any_cast<double>(params["Jxy"]) * (spsm + smsp) +
+                   0.25 * (std::any_cast<double>(params["Jkz"]) * (SzszxId + IdxSzsz) +
+                           0.5 * std::any_cast<double>(params["Jkxy"]) * ((SpsmxId + IdxSpsm) + (SmspxId + IdxSmsp))) +
+                   std::any_cast<double>(params["Iz"]) * SzSz + 0.5 * std::any_cast<double>(params["Ixy"]) * (SpSm + SmSp);
+        }
 
-        auto IdxSs = tprod(B.Id(), std::sqrt(3.) * SiteOperator<double, Symmetry>::prod(B.Sdag(0), B.S(1), Symmetry::qvacuum()));
+        if((bond & Opts::Bond::H) == Opts::Bond::H) {
+            for(auto& t : this->data_h) { t = gate; }
+        }
+        if((bond & Opts::Bond::H) == Opts::Bond::V) {
+            for(auto& t : this->data_v) { t = gate; }
+        }
+        if((bond & Opts::Bond::D1) == Opts::Bond::D1) {
+            for(auto& t : this->data_d1) { t = gate; }
+        }
+        if((bond & Opts::Bond::D2) == Opts::Bond::D2) {
+            for(auto& t : this->data_d2) { t = gate; }
+        }
 
-        auto SsxId = tprod(std::sqrt(3.) * SiteOperator<double, Symmetry>::prod(B.Sdag(0), B.S(1), Symmetry::qvacuum()), B.Id());
-
-        Tensor<double, 2, 2, Symmetry> out = J * ss + 0.25 * Jk * (SsxId + IdxSs) + I * SS;
-        return out;
+        for(auto& t : this->data_h) { t = gate; }
+        for(auto& t : this->data_v) { t = gate; }
     }
 };
 

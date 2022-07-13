543018c2e45f2db9a7f0e31e0a8a165d9c02587d
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-07-13 16:56:36 +0200

    Fixed typo in include statement.

--- include/Xped/PEPS/Models/Heisenberg.hpp
+++ include/Xped/PEPS/Models/Heisenberg.hpp
@@ -6,7 +6,7 @@
 #include <string>
 
 #include "Xped/Core/Tensor.hpp"
-#include "Xped/PEPS/TwoSiteObservable"
+#include "Xped/PEPS/TwoSiteObservable.hpp"
 #include "Xped/Physics/SpinBase.hpp"
 
 namespace Xped {

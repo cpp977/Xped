25c7c7f45ff7f30f843c56a50c3afaa407687aba
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-10-19 11:10:48 +0200

    Added generic stream operator for enums also in Xped namespace.

--- include/Xped/Util/EnumStream.hpp
+++ include/Xped/Util/EnumStream.hpp
@@ -3,7 +3,7 @@
 
 #include <boost/describe.hpp>
 
-namespace Xped::Opts {
+namespace Xped {
 
 template <typename T, typename = typename std::enable_if_t<boost::describe::has_describe_enumerators<T>::value>>
 std::ostream& operator<<(std::ostream& os, const T& t)
@@ -12,6 +12,15 @@ std::ostream& operator<<(std::ostream& os, const T& t)
     return os;
 }
 
-} // namespace Xped::Opts
+namespace Opts {
 
+template <typename T, typename = typename std::enable_if_t<boost::describe::has_describe_enumerators<T>::value>>
+std::ostream& operator<<(std::ostream& os, const T& t)
+{
+    os << boost::describe::enum_to_string(t, "Unknown");
+    return os;
+}
+
+} // namespace Opts
+} // namespace Xped
 #endif

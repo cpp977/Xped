aa5dc1de90de31664a5a5c82587f2ce744e5b946
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-10-26 15:53:39 +0200

    Added `istream` operator for enums using `boost::describe`.

--- include/Xped/Util/EnumStream.hpp
+++ include/Xped/Util/EnumStream.hpp
@@ -12,6 +12,16 @@ std::ostream& operator<<(std::ostream& os, const T& t)
     return os;
 }
 
+template <typename T, typename = typename std::enable_if_t<boost::describe::has_describe_enumerators<T>::value>>
+std::istream& operator>>(std::istream& is, T& t)
+{
+    std::string tmp;
+    is >> tmp;
+    bool success = boost::describe::enum_from_string(tmp.c_str(), t);
+    if(not success) { throw std::runtime_error(std::string("Invalid enumerator name '") + tmp + "'"); }
+    return is;
+}
+
 namespace Opts {
 
 template <typename T, typename = typename std::enable_if_t<boost::describe::has_describe_enumerators<T>::value>>
@@ -19,6 +29,16 @@ std::ostream& operator<<(std::ostream& os, const T& t)
 {
     os << boost::describe::enum_to_string(t, "Unknown");
     return os;
+}
+
+template <typename T, typename = typename std::enable_if_t<boost::describe::has_describe_enumerators<T>::value>>
+std::istream& operator>>(std::istream& is, T& t)
+{
+    std::string tmp;
+    is >> tmp;
+    bool success = boost::describe::enum_from_string(tmp.c_str(), t);
+    if(not success) { throw std::runtime_error(std::string("Invalid enumerator name '") + tmp + "'"); }
+    return is;
 }
 
 } // namespace Opts

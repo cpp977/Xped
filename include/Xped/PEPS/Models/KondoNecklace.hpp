46f4c9cab6ef95aa12518b069fc544274f2d94fb
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2022-11-09 16:23:05 +0100

    Added more observables.

--- include/Xped/PEPS/Models/KondoNecklace.hpp
+++ include/Xped/PEPS/Models/KondoNecklace.hpp
@@ -27,7 +27,7 @@ public:
         , pat(pat_in)
     {
         B = SpinBase<Symmetry>(2, 2);
-        Tensor<double, 2, 2, Symmetry> gate, bond_gate;
+        Tensor<double, 2, 2, Symmetry> gate, local_gate;
         if constexpr(std::is_same_v<Symmetry, Sym::SU2<Sym::SpinSU2>>) {
             used_params = {"J", "Jk", "I"};
         } else {
@@ -43,8 +43,8 @@ public:
 
             auto SsxId = tprod(std::sqrt(3.) * SiteOperator<double, Symmetry>::prod(B.Sdag(0), B.S(1), Symmetry::qvacuum()), B.Id());
 
-            gate = params["J"].get<double>() * ss + 0.25 * params["Jk"].get<double>() * (SsxId + IdxSs) + params["I"].get<double>() * SS;
-            bond_gate = params["J"].get<double>() * ss + params["I"].get<double>() * SS;
+            gate = params["J"].get<double>() * ss + params["I"].get<double>() * SS;
+            local_gate = gate + 0.25 * params["Jk"].get<double>() * (SsxId + IdxSs);
         } else {
             auto SzSz = tprod(B.Sz(0), B.Sz(0));
             auto SpSm = tprod(B.Sp(0), B.Sm(0));
@@ -61,26 +61,23 @@ public:
             auto SzszxId = tprod(B.Sz(0) * B.Sz(1), B.Id());
             auto SpsmxId = tprod(B.Sp(0) * B.Sm(1), B.Id());
             auto SmspxId = tprod(B.Sm(0) * B.Sp(1), B.Id());
-
-            gate = params["Jz"].get<double>() * szsz + 0.5 * params["Jxy"].get<double>() * (spsm + smsp) +
-                   0.25 * (params["Jkz"].get<double>() * (SzszxId + IdxSzsz) +
-                           0.5 * params["Jkxy"].get<double>() * ((SpsmxId + IdxSpsm) + (SmspxId + IdxSmsp))) +
-                   params["Iz"].get<double>() * SzSz + 0.5 * params["Ixy"].get<double>() * (SpSm + SmSp);
-            bond_gate = params["Jz"].get<double>() * szsz + 0.5 * params["Jxy"].get<double>() * (spsm + smsp) + params["Iz"].get<double>() * SzSz +
-                        0.5 * params["Ixy"].get<double>() * (SpSm + SmSp);
+            gate = params["Jz"].get<double>() * szsz + 0.5 * params["Jxy"].get<double>() * (spsm + smsp);
+            gate = gate + params["Iz"].get<double>() * SzSz + 0.5 * params["Ixy"].get<double>() * (SpSm + SmSp);
+            local_gate = gate + 0.25 * (params["Jkz"].get<double>() * (SzszxId + IdxSzsz) +
+                                        0.5 * params["Jkxy"].get<double>() * ((SpsmxId + IdxSpsm) + (SmspxId + IdxSmsp)));
         }
 
         if((bond & Opts::Bond::H) == Opts::Bond::H) {
-            for(auto& t : this->data_h) { t = gate; }
+            for(auto& t : this->data_h) { t = local_gate; }
         }
         if((bond & Opts::Bond::V) == Opts::Bond::V) {
-            for(auto& t : this->data_v) { t = gate; }
+            for(auto& t : this->data_v) { t = local_gate; }
         }
         if((bond & Opts::Bond::D1) == Opts::Bond::D1) {
-            for(auto& t : this->data_d1) { t = bond_gate; }
+            for(auto& t : this->data_d1) { t = gate; }
         }
         if((bond & Opts::Bond::D2) == Opts::Bond::D2) {
-            for(auto& t : this->data_d2) { t = bond_gate; }
+            for(auto& t : this->data_d2) { t = gate; }
         }
     }
 
@@ -90,8 +87,60 @@ public:
         for(auto& t : Sz->data) { t = B.Sz(0).data.template trim<2>(); }
         auto sz = std::make_unique<Xped::OneSiteObservable<Symmetry>>(pat, "sz");
         for(auto& t : sz->data) { t = B.Sz(1).data.template trim<2>(); }
+        auto Szsz = std::make_unique<Xped::OneSiteObservable<Symmetry>>(pat, "Szsz");
+        for(auto& t : Szsz->data) { t = (B.Sz(0) * B.Sz(1)).data.template trim<2>(); }
+        auto Spsm = std::make_unique<Xped::OneSiteObservable<Symmetry>>(pat, "Spsm");
+        for(auto& t : Spsm->data) { t = (B.Sp(0) * B.Sm(1)).data.template trim<2>(); }
+        auto Smsp = std::make_unique<Xped::OneSiteObservable<Symmetry>>(pat, "Smsp");
+        for(auto& t : Smsp->data) { t = (B.Sm(0) * B.Sp(1)).data.template trim<2>(); }
         obs.push_back(std::move(Sz));
         obs.push_back(std::move(sz));
+        obs.push_back(std::move(Szsz));
+        obs.push_back(std::move(Spsm));
+        obs.push_back(std::move(Smsp));
+
+        auto szsz = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "szsz");
+        for(auto& t : szsz->data_h) { t = Xped::tprod(B.Sz(1), B.Sz(1)); }
+        for(auto& t : szsz->data_v) { t = Xped::tprod(B.Sz(1), B.Sz(1)); }
+        for(auto& t : szsz->data_d1) { t = Xped::tprod(B.Sz(1), B.Sz(1)); }
+        for(auto& t : szsz->data_d2) { t = Xped::tprod(B.Sz(1), B.Sz(1)); }
+        auto spsm = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "spsm");
+        for(auto& t : spsm->data_h) { t = Xped::tprod(B.Sp(1), B.Sm(1)); }
+        for(auto& t : spsm->data_v) { t = Xped::tprod(B.Sp(1), B.Sm(1)); }
+        for(auto& t : spsm->data_d1) { t = Xped::tprod(B.Sp(1), B.Sm(1)); }
+        for(auto& t : spsm->data_d2) { t = Xped::tprod(B.Sp(1), B.Sm(1)); }
+        auto smsp = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "smsp");
+        for(auto& t : smsp->data_h) { t = Xped::tprod(B.Sm(1), B.Sp(1)); }
+        for(auto& t : smsp->data_v) { t = Xped::tprod(B.Sm(1), B.Sp(1)); }
+        for(auto& t : smsp->data_d1) { t = Xped::tprod(B.Sm(1), B.Sp(1)); }
+        for(auto& t : smsp->data_d2) { t = Xped::tprod(B.Sm(1), B.Sp(1)); }
+        auto SzSz = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "SzSz");
+        for(auto& t : SzSz->data_h) { t = Xped::tprod(B.Sz(0), B.Sz(0)); }
+        for(auto& t : SzSz->data_v) { t = Xped::tprod(B.Sz(0), B.Sz(0)); }
+        for(auto& t : SzSz->data_d1) { t = Xped::tprod(B.Sz(0), B.Sz(0)); }
+        for(auto& t : SzSz->data_d2) { t = Xped::tprod(B.Sz(0), B.Sz(0)); }
+        auto SpSm = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "SpSm");
+        for(auto& t : SpSm->data_h) { t = Xped::tprod(B.Sp(0), B.Sm(0)); }
+        for(auto& t : SpSm->data_v) { t = Xped::tprod(B.Sp(0), B.Sm(0)); }
+        for(auto& t : SpSm->data_d1) { t = Xped::tprod(B.Sp(0), B.Sm(0)); }
+        for(auto& t : SpSm->data_d2) { t = Xped::tprod(B.Sp(0), B.Sm(0)); }
+        auto SmSp = std::make_unique<Xped::TwoSiteObservable<Symmetry>>(
+            pat, Xped::Opts::Bond::H | Xped::Opts::Bond::V | Xped::Opts::Bond::D1 | Xped::Opts::Bond::D2, "SmSp");
+        for(auto& t : SmSp->data_h) { t = Xped::tprod(B.Sm(0), B.Sp(0)); }
+        for(auto& t : SmSp->data_v) { t = Xped::tprod(B.Sm(0), B.Sp(0)); }
+        for(auto& t : SmSp->data_d1) { t = Xped::tprod(B.Sm(0), B.Sp(0)); }
+        for(auto& t : SmSp->data_d2) { t = Xped::tprod(B.Sm(0), B.Sp(0)); }
+        obs.push_back(std::move(szsz));
+        obs.push_back(std::move(spsm));
+        obs.push_back(std::move(smsp));
+        obs.push_back(std::move(SzSz));
+        obs.push_back(std::move(SpSm));
+        obs.push_back(std::move(SmSp));
     }
 
     virtual std::string file_name() const override { return internal::create_filename("KondoNecklace", params, used_params); }

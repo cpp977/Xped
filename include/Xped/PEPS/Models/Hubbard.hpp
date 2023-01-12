a2d75776c26ca978563c1b46caa1c74cfd170518
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2023-01-12 14:48:38 +0100

    Fixed file messup from last commit.

--- include/Xped/PEPS/Models/Hubbard.hpp
+++ include/Xped/PEPS/Models/Hubbard.hpp
@@ -191,51 +191,52 @@ public:
                 obs.push_back(std::move(SxSx));
             }
         }
+    }
 
-        virtual std::string file_name() const override { return internal::create_filename("Hubbard", params, used_params); }
+    virtual std::string file_name() const override { return internal::create_filename("Hubbard", params, used_params); }
 
-        virtual std::string format() const override
-        {
-            return internal::format_params(fmt::format("Hubbard[sym={}]", Symmetry::name()), params, used_params);
+    virtual std::string format() const override
+    {
+        return internal::format_params(fmt::format("Hubbard[sym={}]", Symmetry::name()), params, used_params);
+    }
+
+    virtual void computeObs(XPED_CONST CTM<double, Symmetry, 2, false, Opts::CTMCheckpoint{}>& env) override
+    {
+        for(auto& ob : obs) {
+            if(auto* one = dynamic_cast<OneSiteObservable<Symmetry>*>(ob.get()); one != nullptr) { avg(env, *one); }
+            if(auto* two = dynamic_cast<TwoSiteObservable<Symmetry>*>(ob.get()); two != nullptr) { avg(env, *two); }
         }
+    }
 
-        virtual void computeObs(XPED_CONST CTM<double, Symmetry, 2, false, Opts::CTMCheckpoint{}> & env) override
-        {
-            for(auto& ob : obs) {
-                if(auto* one = dynamic_cast<OneSiteObservable<Symmetry>*>(ob.get()); one != nullptr) { avg(env, *one); }
-                if(auto* two = dynamic_cast<TwoSiteObservable<Symmetry>*>(ob.get()); two != nullptr) { avg(env, *two); }
-            }
+    virtual void computeObs(XPED_CONST CTM<double, Symmetry, 1, false, Opts::CTMCheckpoint{}>& env) override
+    {
+        for(auto& ob : obs) {
+            if(auto* one = dynamic_cast<OneSiteObservable<Symmetry>*>(ob.get()); one != nullptr) { avg(env, *one); }
+            if(auto* two = dynamic_cast<TwoSiteObservable<Symmetry>*>(ob.get()); two != nullptr) { avg(env, *two); }
         }
+    }
 
-        virtual void computeObs(XPED_CONST CTM<double, Symmetry, 1, false, Opts::CTMCheckpoint{}> & env) override
-        {
-            for(auto& ob : obs) {
-                if(auto* one = dynamic_cast<OneSiteObservable<Symmetry>*>(ob.get()); one != nullptr) { avg(env, *one); }
-                if(auto* two = dynamic_cast<TwoSiteObservable<Symmetry>*>(ob.get()); two != nullptr) { avg(env, *two); }
-            }
+    virtual std::string getObsString(const std::string& offset) const override
+    {
+        std::string out;
+        for(auto i = 0ul; const auto& ob : obs) {
+            out.append(ob->getResString(offset));
+            if(i++ < obs.size() - 1) { out.push_back('\n'); }
         }
+        return out;
+    }
 
-        virtual std::string getObsString(const std::string& offset) const override
-        {
-            std::string out;
-            for(auto i = 0ul; const auto& ob : obs) {
-                out.append(ob->getResString(offset));
-                if(i++ < obs.size() - 1) { out.push_back('\n'); }
-            }
-            return out;
-        }
+    virtual void obsToFile(HighFive::File& file, const std::string& root = "/") const override
+    {
+        for(const auto& ob : obs) { ob->toFile(file, root); }
+    }
 
-        virtual void obsToFile(HighFive::File & file, const std::string& root = "/") const override
-        {
-            for(const auto& ob : obs) { ob->toFile(file, root); }
-        }
-
-        std::map<std::string, Param> params;
-        Pattern pat;
-        std::vector<std::string> used_params;
-        FermionBase<Symmetry> F;
-        std::vector<std::unique_ptr<ObservableBase>> obs;
-    };
+    std::map<std::string, Param> params;
+    Pattern pat;
+    std::vector<std::string> used_params;
+    FermionBase<Symmetry> F;
+    std::vector<std::unique_ptr<ObservableBase>> obs;
+};
 
 } // namespace Xped
 #endif

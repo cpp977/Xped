2fa97408e6665d2222c9213b9bb3659c77b2f4fc
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2023-05-06 21:05:26 +0200

    Removed folder *vcpkg* for doxygen runs.

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -3,6 +3,7 @@ function(enable_doxygen)
     set(DOXYGEN_CALLER_GRAPH YES)
     set(DOXYGEN_CALL_GRAPH YES)
     set(DOXYGEN_EXTRACT_ALL YES)
+    set(DOXYGEN_EXCLUDE "vcpkg")
     find_package(Doxygen REQUIRED dot)
     doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
 

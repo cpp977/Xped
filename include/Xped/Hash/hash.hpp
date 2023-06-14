aa9a23018e3cb3fc0439a2da2f53b9bd458ae215
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2023-06-14 11:48:54 +0200

    Do not open the std namespace explicitly when specializing std::hash.

--- include/Xped/Hash/hash.hpp
+++ include/Xped/Hash/hash.hpp
@@ -15,65 +15,64 @@ struct Permutation;
 }
 } // namespace Xped
 
-namespace std {
-template <size_t Nq>
-struct hash<Xped::qarray<Nq>>
+template <std::size_t Nq>
+struct std::hash<Xped::qarray<Nq>>
 {
-    inline size_t operator()(const Xped::qarray<Nq>& ix) const
+    inline std::size_t operator()(const Xped::qarray<Nq>& ix) const
     {
-        size_t seed = 0;
+        std::size_t seed = 0;
         boost::hash_combine(seed, ix);
         return seed;
     }
 };
 
 /**Hashes an array of quantum numbers using boost's \p hash_combine.*/
-template <size_t Nq, size_t Nlegs>
-struct hash<array<Xped::qarray<Nq>, Nlegs>>
+template <std::size_t Nq, std::size_t Nlegs>
+struct std::hash<std::array<Xped::qarray<Nq>, Nlegs>>
 {
-    inline size_t operator()(const array<Xped::qarray<Nq>, Nlegs>& ix) const
+    inline std::size_t operator()(const std::array<Xped::qarray<Nq>, Nlegs>& ix) const
     {
-        size_t seed = 0;
+        std::size_t seed = 0;
         boost::hash_combine(seed, ix);
         return seed;
     }
 };
 
-template <size_t Rank, typename Symmetry>
-struct hash<Xped::FusionTree<Rank, Symmetry>>
+template <std::size_t Rank, typename Symmetry>
+struct std::hash<Xped::FusionTree<Rank, Symmetry>>
 {
-    inline size_t operator()(const Xped::FusionTree<Rank, Symmetry>& ix) const
+    inline std::size_t operator()(const Xped::FusionTree<Rank, Symmetry>& ix) const
     {
-        size_t seed = 0;
+        std::size_t seed = 0;
         boost::hash_combine(seed, ix);
         return seed;
     }
 };
 
-template <size_t Rank, size_t CoRank, typename Symmetry>
-struct hash<std::pair<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>>>
+template <std::size_t Rank, std::size_t CoRank, typename Symmetry>
+struct std::hash<std::pair<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>>>
 {
-    inline size_t operator()(const std::pair<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>>& ix) const
+    inline std::size_t operator()(const std::pair<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>>& ix) const
     {
-        size_t seed = 0;
+        std::size_t seed = 0;
         boost::hash_combine(seed, ix.first);
         boost::hash_combine(seed, ix.second);
         return seed;
     }
 };
 
-template <size_t Rank, size_t CoRank, typename Symmetry>
-struct hash<std::tuple<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>, Xped::util::Permutation>>
+template <std::size_t Rank, std::size_t CoRank, typename Symmetry>
+struct std::hash<std::tuple<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>, Xped::util::Permutation>>
 {
-    inline size_t
+    inline std::size_t
     operator()(const std::tuple<Xped::FusionTree<Rank, Symmetry>, Xped::FusionTree<CoRank, Symmetry>, Xped::util::Permutation>& ix) const
     {
-        size_t seed = 0;
+        std::size_t seed = 0;
         boost::hash_combine(seed, std::get<0>(ix));
         boost::hash_combine(seed, std::get<1>(ix));
         boost::hash_combine(seed, std::get<2>(ix));
         return seed;
     }
 };
-} // namespace std
+
 #endif

809dc158a32ec20c84d2984addeb3b10964c7a1b
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2023-07-17 10:05:27 +0200

    Added custom port for eigen to have a newer version available.

---
+++ ports/eigen3/portfile.cmake
@@ -0,0 +1,37 @@
+vcpkg_buildpath_length_warning(37)
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.com
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO libeigen/eigen
+    REF 15ac3765c41b39d387736db46607c7b8460710fa
+    SHA512 ecf2ff7408ed96f3699eb5dbe3fd1aa839ac76ff068a5aebb561c7a323f26bf80221b85e6d391adc1c53bff5a4771949197ab3db65931d5296ab0aeb902c43b1
+    HEAD_REF master
+    PATCHES
+        # remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
+        # fix-vectorized-reductions-half.patch # Remove this patch in the next update
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DEIGEN_BUILD_PKGCONFIG=ON
+    OPTIONS_RELEASE
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3
+        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig
+    OPTIONS_DEBUG
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/share/eigen3
+        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
+
+file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
+# Copy the eigen header files to conventional location for user-wide MSBuild integration
+file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

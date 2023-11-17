1c05c54279c638fa03f07829ad584e620059efc0
Matthias Peschke <mpeschke@physnet.uni-hamburg.de>
2023-11-17 12:36:04 +0100

    Added more tests for ad functionality.

--- tests/ad.cpp
+++ tests/ad.cpp
@@ -4,6 +4,7 @@
 
 #include "Xped/Util/Macros.hpp"
 
+#include "Xped/Util/Bool.hpp"
 #include "Xped/Util/Mpi.hpp"
 
 #ifdef XPED_CACHE_PERMUTE_OUTPUT
@@ -53,7 +54,7 @@ TEST_CASE("Testing backwards AD for coeff().")
     SUBCASE("SU2")
     {
         using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
-        auto f = [](const auto& t) { return t.coeff(0, 0, 0); };
+        auto f = [](const auto& t) { return t.coeff(t.sector().size() - 1, 0, 0); };
         Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
     }
     SUBCASE("U1")
@@ -71,7 +72,7 @@ TEST_CASE("Testing backwards AD for coeff().")
 }
 
 #ifdef XPED_USE_MPI
-MPI_TEST_CASE("Testing backwards AD for squareNnorm().", MPI_NUM_PROC)
+MPI_TEST_CASE("Testing backwards AD for squaredNorm().", MPI_NUM_PROC)
 #else
 TEST_CASE("Testing backwards AD for squaredNorm().")
 #endif
@@ -102,6 +103,68 @@ TEST_CASE("Testing backwards AD for squaredNorm().")
 }
 
 #ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for trace().", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for trace().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        auto f = [](const auto& t) { return t.trace(); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) { return t.trace(); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) { return t.trace(); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for twist().", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for twist().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        auto f = [](const auto& t) { return t.twist(0).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) { return t.twist(0).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) { return t.twist(0).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
 MPI_TEST_CASE("Testing backwards AD for norm().", MPI_NUM_PROC)
 #else
 TEST_CASE("Testing backwards AD for norm().")
@@ -115,7 +178,7 @@ TEST_CASE("Testing backwards AD for norm().")
     SUBCASE("SU2")
     {
         using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
-        auto f = [](const auto& t) { return sqrt((t * t.adjoint()).trace()); };
+        auto f = [](const auto& t) { return t.norm(); };
         Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
     }
     SUBCASE("U1")
@@ -159,6 +222,193 @@ TEST_CASE("Testing backwards AD for maxNorm().")
     {
         using Symmetry = Xped::Sym::U0<>;
         auto f = [](const auto& t) { return t.maxNorm(); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for operator*(true,false).", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for operator*().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        auto f = [](const auto& t) {
+            auto c = t.detach();
+            static thread_local std::mt19937 engine(std::random_device{}());
+            engine.seed(100);
+            c.setRandom(engine);
+            return (t * c).coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) {
+            auto c = t.detach();
+            static thread_local std::mt19937 engine(std::random_device{}());
+            engine.seed(100);
+            c.setRandom(engine);
+            return (t * c).coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) {
+            auto c = t.detach();
+            static thread_local std::mt19937 engine(std::random_device{}());
+            engine.seed(100);
+            c.setRandom(engine);
+            return (t * c).coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for operator*(true,ture).", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for operator*().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        auto f = [](const auto& t) { return (t * t).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) { return (t * t).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) { return (t * t).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for permute().", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for permute().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        using Scalar = double;
+        auto f = [](const auto& t) {
+            constexpr auto ENABLE_AD = std::decay_t<decltype(t)>::IS_AD;
+            auto tdag = t.adjoint().eval().template permute<-4, 1, 2, 3, 4, 0>(Xped::Bool<ENABLE_AD>{});
+            Xped::Qbasis<Symmetry> env;
+            Xped::Qbasis<Symmetry> peps = t.uncoupledDomain()[0];
+            env.push_back({1}, 2);
+            env.push_back({2}, 2);
+            env.push_back({3}, 2);
+            Xped::Tensor<double, 2, 0, Symmetry, false> C1init({{env, env}}, {{}});
+            static thread_local std::mt19937 engine(std::random_device{}());
+            engine.seed(0);
+            C1init.setRandom(engine);
+            Xped::Tensor<double, 2, 0, Symmetry, ENABLE_AD> C1 = C1init;
+            Xped::Tensor<double, 4, 0, Symmetry, false> T1init({{env, env, peps, peps}}, {{}});
+            engine.seed(1);
+            T1init.setRandom(engine);
+            Xped::Tensor<double, 4, 0, Symmetry, ENABLE_AD> T1 = T1init;
+
+            auto C1T1 = C1.template contract<std::array{-1, 1}, std::array{1, -2, -3, -4}, 1>(T1.adjoint().eval());
+            auto T4C1T1 = T1.adjoint().eval().template contract<std::array{-1, 1, -2, -3}, std::array{1, -4, -5, -6}, 3>(C1T1.twist(0));
+            auto T4C1T1A = T4C1T1.template contract<std::array{-1, 1, -2, -3, 2, -4}, std::array{1, 2, -5, -6, -7}, 4>(t);
+            auto C4T3 = C1.template contract<std::array{1, -1}, std::array{-2, 1, -3, -4}, 1>(T1.adjoint().eval());
+            auto C4T3Ad = C4T3.template contract<std::array{-1, -2, -3, 1}, std::array{-4, -5, -6, 1, -7}, 3>(tdag.twist(3));
+            auto left_half = T4C1T1A.template contract<std::array{1, 2, -1, 3, -2, 4, -3}, std::array{1, -4, 4, 2, 3, -5, -6}, 3>(C4T3Ad);
+            // return left_half.coeff(0, 0, 0);
+            auto T1C2 = T1.template contract<std::array{-1, 1, -2, -3}, std::array{1, -4}, 3>(C1.adjoint().eval());
+            auto T1C2T2 = T1C2.template contract<std::array{-1, -2, -3, 1}, std::array{1, -4, -5, -6}, 3>(T1);
+            auto AT1C2T2 = t.adjoint().eval().twist(1).template contract<std::array{-3, -1, 1, 2, -2}, std::array{-4, 1, -5, -6, 2, -7}, 3>(T1C2T2);
+            auto T3C3 = T1.template contract<std::array{1, -1, -2, -3}, std::array{-4, 1}, 3>(C1.adjoint().eval());
+            auto T3C3Ad = T3C3.template contract<std::array{-1, -2, 1, -3}, std::array{-4, -5, -6, 1, -7}, 3>(t);
+            auto right_half = AT1C2T2.template contract<std::array{-1, 1, -2, -3, 2, 3, 4}, std::array{-4, 1, 3, -5, 2, 4, -6}, 3>(T3C3Ad);
+            auto rho_h = left_half.template contract<std::array{1, 2, -3, 3, 4, -1}, std::array{2, -4, 1, 3, 4, -2}, 2>(right_half);
+            auto Id2 = Xped::Tensor<Scalar, 2, 2, Symmetry, false>::Identity(rho_h.uncoupledCodomain(), rho_h.uncoupledDomain(), rho_h.world());
+            auto norm = rho_h.template contract<std::array{1, 2, 3, 4}, std::array{3, 4, 1, 2}, 0>(Id2.twist(0).twist(1)).trace();
+            rho_h = rho_h * (1. / norm);
+            // return tmp.coeff(tmp.sector().size() - 1, 0, 0);
+            return rho_h.trace();
+            // return tmp.coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd_small<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) {
+            constexpr auto ENABLE_AD = std::decay_t<decltype(t)>::IS_AD;
+            return t.template permute<-1, 3, 1, 2, 0>(Xped::Bool<ENABLE_AD>{}).coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) {
+            constexpr auto ENABLE_AD = std::decay_t<decltype(t)>::IS_AD;
+            return t.template permute<-1, 3, 1, 2, 0>(Xped::Bool<ENABLE_AD>{}).coeff(0, 0, 0);
+        };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
+    }
+}
+
+#ifdef XPED_USE_MPI
+MPI_TEST_CASE("Testing backwards AD for contract().", MPI_NUM_PROC)
+#else
+TEST_CASE("Testing backwards AD for contract().")
+#endif
+{
+#ifdef XPED_USE_MPI
+    Xped::mpi::XpedWorld test_world(test_comm);
+#else
+    Xped::mpi::XpedWorld test_world;
+#endif
+    SUBCASE("SU2")
+    {
+        using Symmetry = Xped::Sym::SU2<Xped::Sym::SpinSU2>;
+        auto f = [](const auto& t) { return t.template contract<std::array{-1, 1, 2, -2}, std::array{2, 1, -3, -4}, 2>(t).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, SU2_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U1")
+    {
+        using Symmetry = Xped::Sym::U1<Xped::Sym::SpinU1>;
+        auto f = [](const auto& t) { return t.template contract<std::array{-1, -2, 1, 2}, std::array{1, 2, -3, -4}, 2>(t).coeff(0, 0, 0); };
+        Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U1_BASIS_SIZE, test_world);
+    }
+    SUBCASE("U0")
+    {
+        using Symmetry = Xped::Sym::U0<>;
+        auto f = [](const auto& t) { return t.template contract<std::array{-1, 1, 2, -2}, std::array{2, 1, -3, -4}, 2>(t).coeff(0, 0, 0); };
         Xped::internal::check_ad_vs_fd<double, Symmetry>(f, U0_BASIS_SIZE, test_world);
     }
 }
--- tests/ad_tests.hpp
+++ tests/ad_tests.hpp
@@ -13,16 +13,16 @@ void check_ad_vs_fd(const F& f, std::size_t basis_size, Xped::mpi::XpedWorld& wo
     Xped::Qbasis<Symmetry> B1, B2;
     if(world.rank == 0) {
         B1.setRandom(basis_size);
+        B1.sort();
         B2.setRandom(basis_size);
+        B2.sort();
     }
     Xped::mpi::broadcast(B1, world.rank, 0, world);
     Xped::mpi::broadcast(B2, world.rank, 0, world);
-    // Xped::mpi::broadcast(B2, world.rank, 0, world);
-    // std::filesystem::path p("/home/user/__LIBS__/j1j2_ipeps_states/single-site_pg-C4v-A1/j20.0/state_1s_A1_j20.0_D2_chi_opt48.json");
-    // auto tmp = Xped::IO::loadU0JsonTensor<Symmetry>(p);
-    // Xped::Tensor<double, 4, 1, Symmetry, true> t = tmp;
+
     Xped::Tensor<double, 2, 2, Symmetry, true> t({{B1, B2}}, {{B1, B2}}, world);
     t.setRandom();
+
     // t.print(std::cout, true);
     // std::cout << std::endl;
     stan::math::nested_rev_autodiff nested;
@@ -31,10 +31,47 @@ void check_ad_vs_fd(const F& f, std::size_t basis_size, Xped::mpi::XpedWorld& wo
     auto grad_ad = t.adj();
     auto grad_fd = finite_diff_gradient(f, t.val_op());
 
-    // grad_ad.print(std::cout, true);
-    // std::cout << std::endl;
-    // grad_fd.print(std::cout, true);
-    // std::cout << std::endl;
+    grad_ad.print(std::cout, true);
+    std::cout << std::endl;
+    grad_fd.print(std::cout, true);
+    std::cout << std::endl;
+    CHECK((grad_ad - grad_fd).norm() == doctest::Approx(0.));
+}
+
+template <typename Scalar, typename Symmetry, typename F>
+void check_ad_vs_fd_small(const F& f, std::size_t basis_size, Xped::mpi::XpedWorld& world)
+{
+    Xped::Qbasis<Symmetry> B1, B2, B3, B4;
+    if(world.rank == 0) {
+        B1.push_back({1}, 1);
+        B1.push_back({2}, 1);
+        B1.sort();
+        B2.push_back({2}, 1);
+        B2.sort();
+    }
+    Xped::mpi::broadcast(B1, world.rank, 0, world);
+
+    Xped::Tensor<double, 4, 1, Symmetry, true> t({{B1, B1, B1, B1}}, {{B2}}, world);
+    t.setRandom();
+
+    // Xped::Tensor<double, 2, 2, Symmetry, true> t({{B2, B2}}, {{B2, B2}}, world);
+    // t.setIdentity();
+
+    t.print(std::cout, true);
+    std::cout << std::endl;
+    stan::math::nested_rev_autodiff nested;
+    auto res = f(t);
+    stan::math::grad(res.vi_);
+
+    auto grad_ad = t.adj();
+    auto grad_fd = finite_diff_gradient(f, t.val_op());
+
+    grad_ad.print(std::cout, true);
+    std::cout << std::endl;
+    grad_fd.print(std::cout, true);
+    std::cout << std::endl;
+    (grad_ad / grad_fd).eval().print(std::cout, true);
+    std::cout << std::endl;
     CHECK((grad_ad - grad_fd).norm() == doctest::Approx(0.));
 }
 
